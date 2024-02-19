1377952d6f8155e11b839fb8545ef85bb27396d6
ttt92 <tatti0402@gmail.com>
2024-02-19 21:03:02 +0900

    main

---
+++ config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration  firebaseConfig
+
+//import 'firebase/storage'
+
+const config = {
+  apiKey: "AIzaSyBUad0djmW405y3UQpUizi8mxnssD3UDNY",
+  authDomain: "bakooo-chat.firebaseapp.com",
+  databaseURL: "https://bakooo-chat-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bakooo-chat",
+  storageBucket: "bakooo-chat.appspot.com",
+  messagingSenderId: "544670354150",
+  appId: "1:544670354150:web:7115052d78353037be8927"
+};
+firebase.initializeApp(config);
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
